951db827c5262c74bac775f40b53a6e079437ccf
heng.li <liheng@globalegrow.com>
2020-07-29 16:17:49 +0800

    server log

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/DowneyL/the-way-to-gin/pkg/logging"
 	"github.com/DowneyL/the-way-to-gin/pkg/setting"
 	"github.com/DowneyL/the-way-to-gin/routers"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -18,5 +23,23 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	_ = s.ListenAndServe()
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			logging.InfoPrintf("Listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<- quit
+
+	logging.Info("Shutdown Server ...")
+
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFunc()
+	if err := s.Shutdown(ctx); err != nil {
+		logging.InfoPrintf("Server Shutdown", err)
+	}
+
+	logging.Info("Server exiting")
 }
